d23e1301e03eaee9a36d0b9ada119acc2c9c991e
4wqrs12 <forzstreet61@gmail.com>
2025-07-20 00:20:12 -0400

    uploaded files from coding-projects repo

---
+++ civicsproject/script.js
@@ -0,0 +1,19 @@
+
+
+/*
+It is the introduction of harmful materials, known as pollution, into nature
+These pollutants have differents ways of forming, naturally or man-made
+Some natural pollutants is from volcanoes, forest fires, and dust storms
+Some man-made pollutants can come from the gases of motor vehicles, waste from power plants, and soil pollution like pesticides
+
+
+
+
+Air pollution can result in the following:
+
+Climate change, which is the the change in global temperatures and weather patterns
+
+
+
+
+*/ 
